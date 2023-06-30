b4007f2e6133406be7a208221367f7efa4de0853
amit-2000 <amitperane@gmail.com>
2023-06-30 15:41:51 +0530

    link

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 function App() {
   const text = process.env.REACT_APP_TEXT;
   // console.log(text)
+  const url = process.env.REACT_APP_URL;
   return (
     <div className="App">
       <header className="App-header">
@@ -12,7 +13,7 @@ function App() {
         <p>{text}</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href={url}
           target="_blank"
           rel="noopener noreferrer"
         >
